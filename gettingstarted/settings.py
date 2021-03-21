f0b5b1612367b3ba71cfb4a5602ebecd0b197f1a
guillermo asnen <Guillermoasnen@gmail.com>
2021-03-21 18:39:06 -0300

    .

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
-
+# Application definition
+SITE_ID = 1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
