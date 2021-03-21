a32df27296d8f6cc1347f2ea5698c1ed4533a757
guillermo asnen <Guillermoasnen@gmail.com>
2021-03-21 17:58:18 -0300

    modified:   gettingstarted/settings.py 	modified:   gettingstarted/urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,6 +16,8 @@ import hello.views
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
+    # path("administracion", administracion.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
+    path("adm/", include("administr.urls"))
 ]
